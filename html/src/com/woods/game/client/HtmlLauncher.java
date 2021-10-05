4cb60599859fad1430081abecd675cf0b0b51140
Zafaripour <toastyhatesthesea@gmail.com>
2021-10-05 15:32:33 -0700

    Changed HTML deployment resolution

--- html/src/com/woods/game/client/HtmlLauncher.java
+++ html/src/com/woods/game/client/HtmlLauncher.java
@@ -12,7 +12,7 @@ public class HtmlLauncher extends GwtApplication {
                 // Resizable application, uses available space in browser
                 //return new GwtApplicationConfiguration(true);
                 // Fixed size application:
-                return new GwtApplicationConfiguration(800, 480);
+                return new GwtApplicationConfiguration(1400, 768);
         }
 
         @Override
